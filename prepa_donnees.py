e8c59bea025f38b70528377702ecafc8a728ed9c
Melgeorges <melanie.armand@gmail.com>
2020-03-22 20:33:07 +0100

    ajout contraintes equipages.csv mise a jour fichiers avec plus d'infos

--- prepa_donnees.py
+++ prepa_donnees.py
@@ -1,8 +1,3 @@
-import pandas as pd
-from datetime import datetime
-
-# data non anonymisée
-infos = pd.read_csv("/home/melanie/Documents/croixrouge/data/20200323-ResOP consolidée.csv", delimiter=":")
 
 import pandas as pd
 from datetime import datetime
@@ -26,6 +21,9 @@ fle = [True if "Animateur.rice de cours de FLE" in elem else False for elem in c
 perm_soc = [True if "Responsable Permanence Sociale" in elem else False for elem in competence]
 ci = [True if "CI" in elem else False for elem in competence]
 nouv_bene = [True if "Nouveau bénévole" in elem else False for elem in competence]
+infirmier = [True if "Infirmier.e local.e" in elem else False for elem in competence]
+tsa = [True if "TSA / Coreg" in elem else False for elem in competence]
+
 
 
 infos = infos.assign(chauffeur_vl=chauffeur_vl,
@@ -39,13 +37,15 @@ infos = infos.assign(chauffeur_vl=chauffeur_vl,
                      fle=fle,
                      perm_soc=perm_soc,
                      ci=ci,
-                     nouv_bene=nouv_bene)
+                     nouv_bene=nouv_bene,
+                     tsa=tsa,
+                     infirmier=infirmier)
 
 cols = ['Horodateur', 'Adresse e-mail', 'Numéro de téléphone portable', 'Prénom', 'NOM', 'Votre structure de rattachement', 'Unnamed: 6',
         'Cadre ?', 'Secouriste ?', 'Si oui, quelle est-elle ?', 'Quelle sont vos compétences Croix-Rouge ?',
-        'Quelle est votre profession ?', "Êtes-vous muni d'un uniforme en dotation ou devez-vous en récupérer un à votre Unité Locale ?",
+        'Quelle est votre profession ?', "Uniforme ?",
         'chauffeur_vl', 'PSE1', 'PSE2', 'chauf_vpsp', 'maraud', 'psc1', 'log', 'chef_maraud', 'fle', 'perm_soc', 'ci', 'nouv_bene',
-        'Unnamed: 97']
+        'tsa', 'infirmier', 'Unnamed: 97']
 
 mapping_days = {'Lundi': "2020-03-23",
                 'Mardi': "2020-03-24",
@@ -63,74 +63,7 @@ infos_rowed = infos.melt(id_vars=cols,
 
 infos_rowed.Date = [elem.replace(" ", "") for elem in infos_rowed.Date]
 infos_rowed.Date = [elem.replace("[", "") for elem in infos_rowed.Date]
-infos_rowed.Date = [elem.replace(" ", "") for elem in infos_rowed.Date]
-infos_rowed["day_name"] = [elem.split("-")[0] for elem in infos_rowed.Date]
-infos_rowed["day"] = infos_rowed["day_name"].replace(mapping_days)
-infos_rowed["hour_dispo"] = [elem.split("-")[1]+':00' for elem in infos_rowed.Date]
-infos_rowed['dispo'] = infos_rowed[['day', 'hour_dispo']].agg(' '.join, axis=1)
-infos_rowed['anon'] = infos_rowed.NOM.replace(anon_dict)
-
-
-to_send = infos_rowed[['anon', 'chauffeur_vl', 'PSE1', 'PSE2', 'chauf_vpsp', 'maraud', 'psc1', 'log',
-                       'chef_maraud', 'fle', 'perm_soc', 'ci', 'nouv_bene',
-                       'Date', 'Value', 'day_name','dispo']]
-
-to_send.to_csv("anonymise_competence_dispos.csv")
-
-# modif que j'ai réalisées
-competence = infos["Quelle sont vos compétences Croix-Rouge ?"]
-competence = competence.fillna("aucune")
-
-chauffeur_vl = [True if "Chauffeur VL" in elem else False for elem in competence]
-PSE1 = [True if "PSE1" in elem else False for elem in competence]
-PSE2 = [True if "PSE2" in elem else False for elem in competence]
-chauf_vpsp = [True if "Chauffeur VPSP" in elem else False for elem in competence]
-maraud = [True if "Maraudeur.se" in elem else False for elem in competence]
-psc1 = [True if "PSC1" in elem else False for elem in competence]
-log = [True if "Logisticien.ne Croix-Rouge" in elem else False for elem in competence]
-chef_maraud = [True if "Chef.fe d'équipe maraudes" in elem else False for elem in competence]
-fle = [True if "Animateur.rice de cours de FLE" in elem else False for elem in competence]
-perm_soc = [True if "Responsable Permanence Sociale" in elem else False for elem in competence]
-ci = [True if "CI" in elem else False for elem in competence]
-nouv_bene = [True if "Nouveau bénévole" in elem else False for elem in competence]
-
-
-infos = infos.assign(chauffeur_vl=chauffeur_vl,
-                     PSE1=PSE1,
-                     PSE2=PSE2,
-                     chauf_vpsp=chauf_vpsp,
-                     maraud=maraud,
-                     psc1=psc1,
-                     log=log,
-                     chef_maraud=chef_maraud,
-                     fle=fle,
-                     perm_soc=perm_soc,
-                     ci=ci,
-                     nouv_bene=nouv_bene)
-
-cols = ['Horodateur', 'Adresse e-mail', 'Numéro de téléphone portable', 'Prénom', 'NOM', 'Votre structure de rattachement', 'Unnamed: 6',
-        'Cadre ?', 'Secouriste ?', 'Si oui, quelle est-elle ?', 'Quelle sont vos compétences Croix-Rouge ?',
-        'Quelle est votre profession ?', "Êtes-vous muni d'un uniforme en dotation ou devez-vous en récupérer un à votre Unité Locale ?",
-        'chauffeur_vl', 'PSE1', 'PSE2', 'chauf_vpsp', 'maraud', 'psc1', 'log', 'chef_maraud', 'fle', 'perm_soc', 'ci', 'nouv_bene',
-        'Unnamed: 97']
-
-mapping_days = {'Lundi': "2020-03-23",
-                'Mardi': "2020-03-24",
-                'Mercredi': "2020-03-25",
-                'Jeudi': "2020-03-26",
-                'Vendredi': "2020-03-27",
-                'Samedi': "2020-03-28",
-                'Dimanche': "2020-03-29"}
-
-anon_dict = dict([(y,x) for x,y in enumerate(set(infos.NOM))])
-
-infos_rowed = infos.melt(id_vars=cols,
-                         var_name="Date",
-                         value_name="Value")
-
-infos_rowed.Date = [elem.replace(" ", "") for elem in infos_rowed.Date]
-infos_rowed.Date = [elem.replace("[", "") for elem in infos_rowed.Date]
-infos_rowed.Date = [elem.replace(" ", "") for elem in infos_rowed.Date]
+infos_rowed.Date = [elem.replace("]", "") for elem in infos_rowed.Date]
 infos_rowed["day_name"] = [elem.split("-")[0] for elem in infos_rowed.Date]
 infos_rowed["day"] = infos_rowed["day_name"].replace(mapping_days)
 infos_rowed["hour_dispo"] = [elem.split("-")[1]+':00' for elem in infos_rowed.Date]
--- sort_people.py
+++ sort_people.py
@@ -16,7 +16,7 @@ def get_volunteer_skills_and_availability():
             is_pse1, is_pse2, is_chauf_vpsp, is_chauffeur_vl,is_ci
     availability: dictionnary, availability[volunteer] is a 1d array containing 1 if volunteer is there 0 otherwise
     """
-    csvfile= open("anonymise_competence_dispos.csv", mode='r')
+    csvfile= open("competence_dispos.csv", mode='r')
     csv_reader = csv.DictReader(csvfile)
     volunteer_list = []
     date_list = []
@@ -35,7 +35,7 @@ def get_volunteer_skills_and_availability():
         availability[volunteer]= []
 
     #don't know why I need to repoen (oh well...)
-    csvfile= open("anonymise_competence_dispos.csv", mode='r')
+    csvfile= open("competence_dispos.csv", mode='r')
     csv_reader = csv.DictReader(csvfile)
     for row in csv_reader:
         volunteer = row["anon"]
@@ -63,59 +63,64 @@ def keep_one_skill(to_keep: str):
 
 date_list, volunteer_list, skills, availability = get_volunteer_skills_and_availability()
 n_volunteer = len(volunteer_list)
-print("number of volunteer",n_volunteer)
-
-ci = keep_one_skill("is_ci")
-vpsp = keep_one_skill("is_chauf_vpsp")
-pse2 = keep_one_skill("is_pse2")
-
+print("number of volunteer", n_volunteer)
 
 
 # The aim of this plot is to start visualising the data in order to think about how
 # to make groups
-to_plot = pse2
-plot_name = "pse2_dispos"
-n_to_plot = len(to_plot)
+
+def make_a_plot(to_plot, plot_name, n_to_plot):
+    plt.figure(figsize=(25,20))
+    for count, volunteer in enumerate(to_plot):
+        linestyle = "-"
+        color = "gray"
+
+        if skills[volunteer][2] == "is_chauf_vpsp":
+            linestyle ="--"
+        elif skills[volunteer][3] == "is_chauffeur_vl":
+            linestyle = ":"
+
+        if skills[volunteer][4] == "is_ci":
+            color = "red"
+        elif skills[volunteer][1] == "is_pse2":
+            color = "blue"
+        elif skills[volunteer][0] == "is_pse1":
+            color = "green"
+
+        # we will put zero to np.nan in order to make the plot cleaner
+        id= np.where(availability[volunteer]==0)
+        availability[volunteer][id]= np.nan
+
+        plt.plot(date_list,availability[volunteer]+2*count, color=color, linestyle= linestyle)
+        plt.xticks(rotation=90)
+
+    from matplotlib.patches import Patch
+    from matplotlib.lines import Line2D
+
+    legend_elements = [Line2D([0], [0], color='red', linestyle= "-", lw=1, label='ci'),
+                       Line2D([0], [0], color='blue', linestyle= "-", lw=1, label='pse2'),
+                       Line2D([0], [0], color='green', linestyle= "-", lw=1, label='pse1'),
+                       Line2D([0], [0], color='grey', linestyle= "-", lw=1, label='no skills'),
+                       Line2D([0], [0], color='black', linestyle= "-", lw=1, label='pas chauffeur'),
+                       Line2D([0], [0], color='black', linestyle= "--", lw=1, label='chauffeur vpsp'),
+                       Line2D([0], [0], color='black', linestyle= ":", lw=1, label='chauffeur vl')]
+
+    plt.legend(handles=legend_elements, loc='upper right', bbox_to_anchor=(1.1, 1.1))
+    ytick_loc = np.arange(n_to_plot)*2+1
+    plt.yticks(ytick_loc, to_plot)
+    plt.savefig(f'{plot_name}.png',bbox_inches='tight')
+    plt.clf()
+    plt.close()
 
 
-plt.figure(figsize=(25,20))
-for count, volunteer in enumerate(to_plot):
-    linestyle = "-"
-    color = "gray"
-    
-    if skills[volunteer][2] == "is_chauf_vpsp":
-        linestyle ="--"
-    elif skills[volunteer][3] == "is_chauffeur_vl":
-        linestyle = ":"
+ci = keep_one_skill("is_ci")
+make_a_plot(ci, "ci_dispos", len(ci))
 
-    if skills[volunteer][4] == "is_ci":
-        color = "red"
-    elif skills[volunteer][1] == "is_pse2":
-        color = "blue"
-    elif skills[volunteer][0] == "is_pse1":
-        color = "green"
+vpsp = keep_one_skill("is_chauf_vpsp")
+make_a_plot(vpsp, "vpsp_dispos", len(vpsp))
 
-    # we will put zero to np.nan in order to make the plot cleaner
-    id= np.where(availability[volunteer]==0)
-    availability[volunteer][id]= np.nan
-        
-    plt.plot(date_list,availability[volunteer]+2*count, color=color, linestyle= linestyle)
-    plt.xticks(rotation=90)
-    
-from matplotlib.patches import Patch
-from matplotlib.lines import Line2D
+pse2 = keep_one_skill("is_pse2")
+make_a_plot(pse2, "pse2_dispos", len(pse2))
 
-legend_elements = [Line2D([0], [0], color='red', linestyle= "-", lw=1, label='ci'),
-                   Line2D([0], [0], color='blue', linestyle= "-", lw=1, label='pse2'),
-                   Line2D([0], [0], color='green', linestyle= "-", lw=1, label='pse1'),
-                   Line2D([0], [0], color='grey', linestyle= "-", lw=1, label='no skills'),
-                   Line2D([0], [0], color='black', linestyle= "-", lw=1, label='pas chauffeur'),
-                   Line2D([0], [0], color='black', linestyle= "--", lw=1, label='chauffeur vpsp'),
-                   Line2D([0], [0], color='black', linestyle= ":", lw=1, label='chauffeur vl')]
 
-plt.legend(handles=legend_elements, loc='upper right', bbox_to_anchor=(1.1, 1.1))
-ytick_loc = np.arange(n_to_plot)*2+1
-plt.yticks(ytick_loc, to_plot)
-plt.savefig(f'{plot_name}.png',bbox_inches='tight')
-plt.clf()
-plt.close()
+
