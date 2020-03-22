68b0a19a108b95192876540a34cf7c69515ab23c
Melgeorges <melanie.armand@gmail.com>
2020-03-22 13:50:50 +0100

    initial data and prep code

---
+++ prepa_donnees.py
@@ -0,0 +1,100 @@
+import pandas as pd
+from datetime import datetime
+
+# data non anonymisée
+infos = pd.read_csv("/home/melanie/Documents/croixrouge/data/20200323-ResOP consolidée.csv", delimiter=":")
+anon_dict = dict([(y,x) for x,y in enumerate(set(infos.NOM))])
+infos['anon'] = infos.NOM.replace(anon_dict)
+
+
+to_send = infos[['anon',
+                       'Quelle sont vos compétences Croix-Rouge ?',
+                       'Lundi - [00 - 02h]', 'Lundi - [02 - 04h]', 'Lundi - [04 - 06h]',
+                       'Lundi - [06 - 08h]', 'Lundi - [08 - 10h]', 'Lundi - [10 - 12h]',
+                       'Lundi - [12 - 14h]', 'Lundi - [14 - 16h]', 'Lundi - [16 - 18h]',
+                       'Lundi - [18 - 20h]', 'Lundi - [20 - 22h]', 'Lundi - [22 - 24h]',
+                       'Mardi - [00 - 02h]', 'Mardi - [02 - 04h]', 'Mardi - [04 - 06h]',
+                       'Mardi - [06 - 08h]', 'Mardi - [08 - 10h]', 'Mardi - [10 - 12h]',
+                       'Mardi - [12 - 14h]', 'Mardi - [14 - 16h]', 'Mardi - [16 - 18h]',
+                       'Mardi - [18 - 20h]', 'Mardi - [20 - 22h]', 'Mardi - [22 - 24h]',
+                       'Mercredi - [00 - 02h]', 'Mercredi - [02 - 04h]',
+                       'Mercredi - [04 - 06h]', 'Mercredi - [06 - 08h]',
+                       'Mercredi - [08 - 10h]', 'Mercredi - [10 - 12h]',
+                       'Mercredi - [12 - 14h]', 'Mercredi - [14 - 16h]',
+                       'Mercredi - [16 - 18h]', 'Mercredi - [18 - 20h]',
+                       'Mercredi - [20 - 22h]', 'Mercredi - [22 - 24h]', 'Jeudi - [00 - 02h]',
+                       'Jeudi - [02 - 04h]', 'Jeudi - [04 - 06h]', 'Jeudi - [06 - 08h]',
+                       'Jeudi - [08 - 10h]', 'Jeudi - [10 - 12h]', 'Jeudi - [12 - 14h]',
+                       'Jeudi - [14 - 16h]', 'Jeudi - [16 - 18h]', 'Jeudi - [18 - 20h]',
+                       'Jeudi - [20 - 22h]', 'Jeudi - [22 - 24h]', 'Vendredi - [00 - 02h]',
+                       'Vendredi - [02 - 04h]', 'Vendredi - [04 - 06h]']]
+
+to_send.to_csv("data_initial.csv")
+
+
+# modif que j'ai réalisées
+competence = infos["Quelle sont vos compétences Croix-Rouge ?"]
+competence = competence.fillna("aucune")
+
+chauffeur_vl = [True if "Chauffeur VL" in elem else False for elem in competence]
+PSE1 = [True if "PSE1" in elem else False for elem in competence]
+PSE2 = [True if "PSE2" in elem else False for elem in competence]
+chauf_vpsp = [True if "Chauffeur VPSP" in elem else False for elem in competence]
+maraud = [True if "Maraudeur.se" in elem else False for elem in competence]
+psc1 = [True if "PSC1" in elem else False for elem in competence]
+log = [True if "Logisticien.ne Croix-Rouge" in elem else False for elem in competence]
+chef_maraud = [True if "Chef.fe d'équipe maraudes" in elem else False for elem in competence]
+fle = [True if "Animateur.rice de cours de FLE" in elem else False for elem in competence]
+perm_soc = [True if "Responsable Permanence Sociale" in elem else False for elem in competence]
+ci = [True if "CI" in elem else False for elem in competence]
+nouv_bene = [True if "Nouveau bénévole" in elem else False for elem in competence]
+
+
+infos = infos.assign(chauffeur_vl=chauffeur_vl,
+                     PSE1=PSE1,
+                     PSE2=PSE2,
+                     chauf_vpsp=chauf_vpsp,
+                     maraud=maraud,
+                     psc1=psc1,
+                     log=log,
+                     chef_maraud=chef_maraud,
+                     fle=fle,
+                     perm_soc=perm_soc,
+                     ci=ci,
+                     nouv_bene=nouv_bene)
+
+cols = ['Horodateur', 'Adresse e-mail', 'Numéro de téléphone portable', 'Prénom', 'NOM', 'Votre structure de rattachement', 'Unnamed: 6',
+        'Cadre ?', 'Secouriste ?', 'Si oui, quelle est-elle ?', 'Quelle sont vos compétences Croix-Rouge ?',
+        'Quelle est votre profession ?', "Êtes-vous muni d'un uniforme en dotation ou devez-vous en récupérer un à votre Unité Locale ?",
+        'chauffeur_vl', 'PSE1', 'PSE2', 'chauf_vpsp', 'maraud', 'psc1', 'log', 'chef_maraud', 'fle', 'perm_soc', 'ci', 'nouv_bene',
+        'Unnamed: 97']
+
+mapping_days = {'Lundi': "2020-03-23",
+                'Mardi': "2020-03-24",
+                'Mercredi': "2020-03-25",
+                'Jeudi': "2020-03-26",
+                'Vendredi': "2020-03-27",
+                'Samedi': "2020-03-28",
+                'Dimanche': "2020-03-29"}
+
+anon_dict = dict([(y,x) for x,y in enumerate(set(infos.NOM))])
+
+infos_rowed = infos.melt(id_vars=cols,
+                         var_name="Date",
+                         value_name="Value")
+
+infos_rowed.Date = [elem.replace(" ", "") for elem in infos_rowed.Date]
+infos_rowed.Date = [elem.replace("[", "") for elem in infos_rowed.Date]
+infos_rowed.Date = [elem.replace(" ", "") for elem in infos_rowed.Date]
+infos_rowed["day_name"] = [elem.split("-")[0] for elem in infos_rowed.Date]
+infos_rowed["day"] = infos_rowed["day_name"].replace(mapping_days)
+infos_rowed["hour_dispo"] = [elem.split("-")[1]+':00' for elem in infos_rowed.Date]
+infos_rowed['dispo'] = infos_rowed[['day', 'hour_dispo']].agg(' '.join, axis=1)
+infos_rowed['anon'] = infos_rowed.NOM.replace(anon_dict)
+
+
+to_send = infos_rowed[['anon', 'chauffeur_vl', 'PSE1', 'PSE2', 'chauf_vpsp', 'maraud', 'psc1', 'log',
+                       'chef_maraud', 'fle', 'perm_soc', 'ci', 'nouv_bene',
+                       'Date', 'Value', 'day_name','dispo']]
+
+to_send.to_csv("anonymise_competence_dispos.csv")
