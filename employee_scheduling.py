063520e25c15a92f21445d238c97d780386def13
Guillaume Vernade <Giom-V@users.noreply.github.com>
2020-03-22 15:03:49 -0400

    Adding more contraints (CR chez vous)

--- employee_scheduling.py
+++ employee_scheduling.py
@@ -4,13 +4,13 @@ from sort_people import get_volunteer_skills_and_availability
 
 
 
-class NursesPartialSolutionPrinter(cp_model.CpSolverSolutionCallback):
+class VolunteersPartialSolutionPrinter(cp_model.CpSolverSolutionCallback):
     """Print intermediate solutions."""
 
-    def __init__(self, shifts, num_nurses, num_days, num_shifts, sols):
+    def __init__(self, shifts, num_volunteers, num_days, num_shifts, sols):
         cp_model.CpSolverSolutionCallback.__init__(self)
         self._shifts = shifts
-        self._num_nurses = num_nurses
+        self._num_volunteers = num_volunteers
         self._num_days = num_days
         self._num_shifts = num_shifts
         self._solutions = set(sols)
@@ -21,14 +21,14 @@ class NursesPartialSolutionPrinter(cp_model.CpSolverSolutionCallback):
             print('Solution %i' % self._solution_count)
             for d in range(self._num_days):
                 print('Day %i' % d)
-                for n in range(self._num_nurses):
+                for n in range(self._num_volunteers):
                     is_working = False
                     for s in range(self._num_shifts):
                         if self.Value(self._shifts[(n, d, s)]):
                             is_working = True
-                            print('  Nurse %i works shift %i' % (n, s))
-                    if not is_working:
-                        print('  Nurse {} does not work'.format(n))
+                            print('  Volunteer %i works shift %i' % (n, s))
+                    # if not is_working:
+                    #     print('  Volunteer {} does not work'.format(n))
             print()
         self._solution_count += 1
 
@@ -48,7 +48,7 @@ def main():
     num_volunteers = len(volunteer_list)
     num_shifts = 12
     num_days = int(len(date_list)/num_shifts)
-    # all_nurses = range(num_nurses)
+    # all_volunteers = range(num_volunteers)
     # all_shifts = range(num_shifts)
     # all_days = range(num_days)
     all_volunteers = range(num_volunteers)
@@ -75,32 +75,45 @@ def main():
                 model.Add(sum(shifts[(n, d, s)] for n, volunteer in enumerate(volunteer_list) if skills[volunteer][4]=="is_ci") == 1)
                 model.Add(sum(shifts[(n, d, s)] for n, volunteer in enumerate(volunteer_list) if skills[volunteer][2]=="is_chauf_vpsp") == 1)
                 model.Add(sum(shifts[(n, d, s)] for n, volunteer in enumerate(volunteer_list) if skills[volunteer][1]=="is_pse2") == 1 or sum(shifts[(n, d, s)] for n, volunteer in enumerate(volunteer_list) if skills[volunteer][1]=="is_pse2") == 2)
+    # VLUMS : plage horaire de 6 à 12h - 2 PSE(1 ou 2) - idealement un chauffeur_vl
+    # for d in all_days:
+    #     for s in all_shifts:
+    #         if s>=3 and s<=5:
+    #             model.Add(sum(shifts[(n, d, s)] for n, volunteer in enumerate(volunteer_list) if skills[volunteer][3]=="is_chauffeur_vl") == 1) #TODO: "idéalement"
+    #             model.Add(sum(shifts[(n, d, s)] for n, volunteer in enumerate(volunteer_list) if skills[volunteer][1]=="is_pse2" or skills[volunteer][0]=="is_pse1") == 2)
+
+    # croix rouge chez vous : plage horaire de 4 à 12h - 2 benevoles (pas de compétences particulières) - idealement un chauffeur_vl
+    for d in all_days:
+        for s in all_shifts:
+            if s>=2 and s<=5:
+                model.Add(sum(shifts[(n, d, s)] for n, volunteer in enumerate(volunteer_list) if skills[volunteer][3]=="is_chauffeur_vl") == 1) #TODO: "idéalement"
+                model.Add(sum(shifts[(n, d, s)] for n, volunteer in enumerate(volunteer_list) ) == 2)
 
     # Each volunteer works at most four shift per day.
     for n in all_volunteers:
         for d in all_days:
             model.Add(sum(shifts[(n, d, s)] for s in all_shifts) <= 4)
 
-    # min_shifts_per_nurse is the largest integer such that every nurse
-    # can be assigned at least that many shifts. If the number of nurses doesn't
+    # min_shifts_per_volunteer is the largest integer such that every volunteer
+    # can be assigned at least that many shifts. If the number of volunteers doesn't
     # divide the total number of shifts over the schedule period,
-    # some nurses have to work one more shift, for a total of
-    # min_shifts_per_nurse + 1.
+    # some volunteers have to work one more shift, for a total of
+    # min_shifts_per_volunteer + 1.
 
-    # min_shifts_per_nurse = (num_shifts * num_days) // num_nurses
-    # max_shifts_per_nurse = min_shifts_per_nurse + 1
-    # for n in all_nurses:
+    # min_shifts_per_volunteer = (num_shifts * num_days) // num_volunteers
+    # max_shifts_per_volunteer = min_shifts_per_volunteer + 1
+    # for n in all_volunteers:
     #     num_shifts_worked = sum(
     #         shifts[(n, d, s)] for d in all_days for s in all_shifts)
-    #     model.Add(min_shifts_per_nurse <= num_shifts_worked)
-    #     model.Add(num_shifts_worked <= max_shifts_per_nurse)
+    #     model.Add(min_shifts_per_volunteer <= num_shifts_worked)
+    #     model.Add(num_shifts_worked <= max_shifts_per_volunteer)
 
     # Creates the solver and solve.
     solver = cp_model.CpSolver()
     solver.parameters.linearization_level = 0
-    # Display the first five solutions.
-    a_few_solutions = range(5)
-    solution_printer = NursesPartialSolutionPrinter(shifts, num_volunteers,
+    # Display the first solution.
+    a_few_solutions = range(1)
+    solution_printer = VolunteersPartialSolutionPrinter(shifts, num_volunteers,
                                                     num_days, num_shifts,
                                                     a_few_solutions)
     solver.SearchForAllSolutions(model, solution_printer)
