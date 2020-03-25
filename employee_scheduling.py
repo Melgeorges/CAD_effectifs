b9cff21f53441682d77bec9ae377e1a09929000e
SCERRI Guillaume <guillaume.scerri@uvsq.fr>
2020-03-25 01:47:42 +0100

    un peu de débug

--- employee_scheduling.py
+++ employee_scheduling.py
@@ -8,14 +8,14 @@ class Volunteer:
     def __init__(self, identity, skills, availability):
         self.identity = identity
         # skills order is is_pse1, is_pse2, is_chauf_vpsp, is_chauffeur_vl,is_ci
-        self.pse1 = True if skills[0] == "is_pse1" else False
-        self.pse2 = True if skills[1] == "is_pse2" else False
-        self.chauf_vpsp = True if skills[2] == "is_chauf_vpsp" else False
-        self.chauf_vl = True if skills[3] == "is_chauf_vl" else False
-        self.ci = True if skills[4] == "is_ci" else False
-        self.tsa = True if skills[5] == "is_tsa" else False
-        self.log = True if skills[7] == "is_log" else False
-        self.infirmier = True if skills[6] == "is_infirmier" else False
+        self.pse1 = True if skills[0] == "pse1" else False
+        self.pse2 = True if skills[1] == "pse2" else False
+        self.chauf_vpsp = True if skills[2] == "chauf_vpsp" else False
+        self.chauf_vl = True if skills[3] == "chauf_vl" else False
+        self.ci = True if skills[4] == "ci" else False
+        self.tsa = True if skills[5] == "tsa" else False
+        self.log = True if skills[7] == "log" else False
+        self.infirmier = True if skills[6] == "infirmier" else False
         self.availability = availability
 
     def __repr__(self):
@@ -48,7 +48,7 @@ def create_model(model, volunteer_list, date_list, shift_list):
         identity_list.append(v.identity)
 
     assignment = {}
-    date_list = list(set([d.split(" ")[0] for d in date_list]))
+    #date_list = list(set([d.split(" ")[0] for d in date_list]))
     # creation of boolean variables 1 variable/(volunteer, date, shift where the volunteer is available)
     for v in volunteer_list:
         for d in date_list:
@@ -75,7 +75,10 @@ def create_model(model, volunteer_list, date_list, shift_list):
                           volunteer_dict[identity].log and s.name in volunteer_dict[identity].availability[d]) == s.log)
             model.Add(sum(assignment[(identity, d, s.name)] for identity, identity in enumerate(identity_list) if
                           s.name in volunteer_dict[identity].availability[d]) == s.noskills)
-
+    print(assignment)
+    print(date_list)
+    print(shift_list)
+    print(volunteer_list)
     return assignment
 
 
@@ -114,8 +117,10 @@ class VolunteersPartialSolutionPrinter(cp_model.CpSolverSolutionCallback):
 def main():
     # Data.
     date_list, volunteer_list, skills, availability = get_volunteer_skills_and_availability()
+    print(skills)
     availability = get_shifts_availability(date_list, volunteer_list, skills, availability)
     donotuse, shifts = create_shifts()
+    date_list = [datetime(2020, 3, 29)]
     volunteers = create_volunteers(volunteer_list, skills, availability)
     volunteer_dict = {vol.identity: vol for vol in volunteers}
     # print("date_list",len(date_list),date_list)
